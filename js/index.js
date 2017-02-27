55afb4767db5987977774b6573224891cc67f0e0
flashbax <jkiwtco@gmail.com>
2017-02-27 15:52:43 -0500

    Commit 2

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
-window.onload = function() {
+//window.onload = function() {
+    
+    //object 
+    // var formEntry = {
+    //      firstName: first,
+    //      lastName: last
+    //  }
+    
+    //document.getElementById("firstName").innerHTML = formEntry.firstName;
+    //document.getElementById("lastName").innerHTML = formEntry.lastName;
     
     document.getElementById('submit').addEventListener("click", submit);
     
     function submit() { 
-        alert('works');
+        var firstName = document.querySelector('#firstName').value;
+        alert(firstName);
     }
     
-}; //window load end
+
+
+//}; //window load end
 
 
     
