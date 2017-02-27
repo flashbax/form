12af4e8c74c9e8aa6efc1c5cc34c2e12962e09f5
flashbax <jkiwtco@gmail.com>
2017-02-27 14:55:09 -0500

    Commit 1

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    
+    document.getElementById('submit').addEventListener("click", submit);
+    
+    function submit() { 
+        alert('works');
+    }
+    
+}; //window load end
+
+
+    
