9ef227ac77e123defe9a5cd6937e3e3f315d1c4b
flashbax <jkiwtco@gmail.com>
2017-02-28 17:05:16 -0500

    Commit 6

--- js/index.js
+++ js/index.js
@@ -7,8 +7,7 @@
             var firstName = document.querySelector('#firstName').value;
             var lastName = document.querySelector('#lastName').value;
 
-            return firstName, lastName;
-             
+            return firstName, lastName;    
         }
         
         //object creation of user
@@ -17,9 +16,21 @@
             lName : lastName.value  
         }
         
+        function hasNum() {
+            if (fields.fName.match(/^[0-9]+$/)) { 
+                alert('Names cannot have numbers!');
+            }
+        }
+
+        function isEmpty() {
+            if (fields.fName === "") { 
+                alert('Please Enter Your Name');
+            }
+        }
         console.log(fields.fName + ' ' + fields.lName);
        //fields.fName + ' ' + fields.lName
-        
+        isEmpty();
+        hasNum();
         event.preventDefault(); //keeps browser from submitting. Remove after testing 
     }
 //}; //window load end
