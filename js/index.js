887f98ddcd02a8fee97b233ebb87ac06b725f097
Justin Kwitco <Justin Kwitco>
2017-02-27 19:57:50 -0500

    Commit 4

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 //window.onload = function() {
     document.getElementById('submit').addEventListener("click", submit);
     
-    function submit() { 
-        function getFields() { 
+    function submit() { //submit form
+        function getFields() { //get the input from all fields
             var firstName = document.querySelector('#firstName').value;
             var lastName = document.querySelector('#lastName').value;
 
             return firstName, lastName;
-             //keeps browser from submitting. Remove after testing 
+             
         }
 
         var fields = {
@@ -15,11 +15,12 @@
             lName: lastName.value  
         }
 
+        //add form input to fields object
         fields.fName = firstName.value;
         fields.lName = lastName.value;
         
         console.log(fields.fName + fields.lName);
-        event.preventDefault();
+        event.preventDefault(); //keeps browser from submitting. Remove after testing 
     }
 //}; //window load end
 
